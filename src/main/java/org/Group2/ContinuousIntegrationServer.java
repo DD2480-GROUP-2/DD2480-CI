8deadd788ecf60d4c6c1d5fde774d5a9198ddb52
snumlautoken <arami@live.se>
2023-02-05 21:55:58 +0100

    (fix) #14 Fix dependencies

--- src/main/java/org/Group2/ContinuousIntegrationServer.java
+++ src/main/java/org/Group2/ContinuousIntegrationServer.java
@@ -1,16 +1,18 @@
 package org.Group2; 
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
 import java.io.IOException;
- 
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class ContinuousIntegrationServer extends AbstractHandler {
-    
-    public void handle(String target, Request baseRequest, jakarta.servlet.http.HttpServletRequest request,
-            jakarta.servlet.http.HttpServletResponse response) throws IOException, jakarta.servlet.ServletException {
-        // TODO Auto-generated method stub
-        
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
     }
 
     // TODO!
@@ -27,14 +29,13 @@ public class ContinuousIntegrationServer extends AbstractHandler {
 
     // TODO!
     public void sendResponse(){}
-    
- 
+
+
     public static void main(String[] args) throws Exception {
         Server server = new Server(8002);
         server.setHandler(new ContinuousIntegrationServer()); 
         server.start();
         server.join();
     }
-
 
 }
