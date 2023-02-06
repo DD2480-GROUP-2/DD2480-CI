0d1ec82089138bda4030c9cf88e75be6d4373515
snumlautoken <arami@live.se>
2023-02-06 14:08:04 +0100

    (feat) #4 add basic handle method

--- src/main/java/org/Group2/ContinuousIntegrationServer.java
+++ src/main/java/org/Group2/ContinuousIntegrationServer.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import org.eclipse.jetty.server.Server;
@@ -12,11 +13,22 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class ContinuousIntegrationServer extends AbstractHandler {
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
 
+        System.out.println(target);
+
+        // here you do all the continuous integration tasks
+        // for example
+        // 1st clone your repository
+        // 2nd compile the code
+
+        parseJSON(request.getReader());
     }
 
     // TODO!
-    public void parseJSON(){}
+    public void parseJSON(BufferedReader reader){}
 
     // TODO!
     public void getRepoURL(){}
