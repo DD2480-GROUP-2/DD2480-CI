985a5c0a336174909d12f80476bb063a99f5f851
Oskar Svanström <oskarsva@kth.se>
2023-02-06 17:05:57 +0100

    (bugfix) #7  Fixed duplicate repo cloning
    Function now deletes all files in clonedRepo folder so that a repository can be freshly cloned!

--- src/main/java/org/Group2/ContinuousIntegrationServer.java
+++ src/main/java/org/Group2/ContinuousIntegrationServer.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 
 import java.io.IOException;
 
+import java.io.File;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -21,6 +23,19 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     // TODO!
     public void getRepoURL(){}
 
+
+    /**
+     * Recursively deletes all files in the repo
+     * @param file
+     */
+    public static void deleteDirectory(File file) {
+        for (File subfile : file.listFiles()) {
+            if (subfile.isDirectory()) {
+                deleteDirectory(subfile);
+            }
+            subfile.delete();
+        }
+    }
     /**
      * Clones a github repository to the folder ./clonedRepo that can later be built and tested.
      * @param repoURL the destination of the repository to be cloned
@@ -28,6 +43,10 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     */
     public static void cloneRepo(String repoURL) throws Exception {
         try {
+            File file = new File("./clonedRepo");
+            deleteDirectory(file);
+            file.delete();
+
             Process process=Runtime.getRuntime().exec("git clone "+repoURL+ " ./clonedRepo");
             process.waitFor();
         } catch (IOException | InterruptedException e) {
