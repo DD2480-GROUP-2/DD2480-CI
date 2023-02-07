825207520f484338852c45b7f87cb69c21363a5e
Jakob Ewaldsson <jakobewaldsson@gmail.com>
2023-02-07 16:26:08 +0100

    (feat) #9 Implemented maven compilation function, and a helper function for it

--- src/main/java/org/Group2/ContinuousIntegrationServer.java
+++ src/main/java/org/Group2/ContinuousIntegrationServer.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
+import java.io.File;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
@@ -24,8 +25,36 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     // TODO!
     public void cloneRepo(){}
 
-    // TODO!
-    public void buildAndTestRepo(){} // Might be two separate methods?
+    /**
+     * Executes a given "sh" shell command in a specified directory.
+     * @param command a string containing the shell command
+     * @param path a string specifying the directory
+     * @return exit value/code of the process
+     */
+    public int runCommand(String command, String path){
+        ProcessBuilder pb = new ProcessBuilder("sh", "-c", command);
+        pb.directory(new File(path));
+
+        try {
+            Process process = pb.start();
+            int exitValue = process.waitFor();
+            return exitValue;
+        } catch (Exception e){
+            e.printStackTrace();
+            return 1;
+        }
+
+    }
+
+    /**
+     * Compiles a Maven project located in a specified directory.
+     * @param path a string specifying the directory
+     * @return a boolean value which is true if the compile was successfil, and false otherwise
+     */
+    public boolean compileMvnProject(String path) {
+        int compileStatus = this.runCommand("mvn clean compile", path);
+        return compileStatus == 0;
+    }
 
     // TODO!
     public void sendResponse(){}
